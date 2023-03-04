38f21227a56c89f59686e7c06dd421909016bc39
cool9203 <octer18@gmail.com>
2023-03-04 13:17:56 +0800

    Update: rewrite. use page url to save url_id

--- src/background.js
+++ src/background.js
@@ -31,33 +31,14 @@ const PROD_COUNT_REGEX = /[0-9]+/;
 const PROD_REGEX = /\[.*\]/;
 
 // Program variable
-let category_id = null;
-let prod_count = null;
-let crawler_data = {};
-let all_intersection_id_array = [];
-let origin_url = null;
-
-
-// TODO: 需要大幅改寫, 根據 focus tab url 來建立 intersection_id_array, 並共用 crawl data
-
-
-/** 
- * load jsonp
- * 雖然叫 jsonp, 但實際上就是讀取 js 檔案
- * @params {string} url 要讀取的 js 檔案 url
- */ 
-async function _call_api(url, parse_regex){
-    console.debug(`call api: ${url}`);
-    let response = await fetch(url);
-    let data = await response.text();
-    return parse_regex.exec(data);
-}
+let data_cache = {};
+let all_intersection_id_array = {};
 
 
 /** 
  * 取得商品 id
- * @params {object} data 商品資料, 根據 pchome API 的 PROD_API_URL 來的
- * @returns string 商品 id
+ * @param {object} data 商品資料, 根據 pchome API 的 PROD_API_URL 來的
+ * @return {string} 商品 id
  */ 
 function get_id(data){
     let split_id = data.Id.split("-");
@@ -67,10 +48,10 @@ function get_id(data){
 
 /** 
  * 定義 pchome API 拉回來的資料要取得那些, 資料來源為 PROD_API_URL
- * @params {string} cid category id, 該商品所在的主題 id
- * @params {string} pid prod id, 該商品的 id
- * @params {object} data 商品資料, 根據 pchome API 的 PROD_API_URL 來的
- * @returns string 商品 id
+ * @param {string} cid category id, 該商品所在的主題 id
+ * @param {string} pid prod id, 該商品的 id
+ * @param {object} data 商品資料, 根據 pchome API 的 PROD_API_URL 來的
+ * @return {string} 商品 id
  */ 
 function get_data(cid, pid, data){
     let url = PROD_URL.replace(REPLACE_CATEGORY, cid).replace(REPLACE_PROD, pid); // pchome 的商品頁面連結
@@ -87,10 +68,10 @@ function get_data(cid, pid, data){
 
 /** 
  * 取得兩主題的商品交集
- * @params {object} result 先前聯集完的結果
- * @params {object} data 爬蟲過後的結果
- * @params {array} intersection_id_array 要交集的 id array
- * @returns object 交集後的結果, 資料型態為: {get_id(): get_data()}
+ * @param {object} result 先前聯集完的結果
+ * @param {object} data 爬蟲過後的結果
+ * @param {array} intersection_id_array 要交集的 id array
+ * @return {object} 交集後的結果, 資料型態為: {get_id(): get_data()}
  */ 
 function intersection(result, data, intersection_id_array){
     let first_empty = Object.keys(result).length === 0 ? true : false;
@@ -127,9 +108,9 @@ function intersection(result, data, intersection_id_array){
 
 /** 
  * 取得兩主題的商品聯集
- * @params {object} data 爬蟲過後的結果
- * @params {array} union_id_array 要聯集的 id array
- * @returns object 交集後的結果, 資料型態為: {get_id(): get_data()}
+ * @param {object} data 爬蟲過後的結果
+ * @param {array} union_id_array 要聯集的 id array
+ * @return {object} 交集後的結果, 資料型態為: {get_id(): get_data()}
  */ 
 function union(data, union_id_array){
     let result = {};
@@ -148,122 +129,114 @@ function union(data, union_id_array){
 
 
 /** 
- * 商品資料爬蟲
- * 請注意: 
- * 依賴 crawler_data 與 category_id 這兩個變數
- * 該 callback 應該要同步執行, 才可以正確的將爬蟲回來的商品資料對應到 category_id
- * 否則將會得到錯誤的結果.
- * @params {object} data 商品資料, 根據 pchome API 的 PROD_API_URL 來的
+ * 使用 fetch call API
+ * @param {string} API url
+ * @param {parse_regex} 
+ * @return {string} API response string
  */ 
-function api_prod_callback(data){
-    console.debug("start api_prod_callback");
-    for (let j = 0; j < data.length; j = j + 1){
-        if (!(category_id in crawler_data)){
-            crawler_data[category_id] = [];
-        }
-        crawler_data[category_id].push(data[j]);
+async function _call_api(url, parse_regex=undefined){
+    console.debug(`call api: ${url}`);
+    let response = await fetch(url);
+    let data = await response.text();
+
+    if (parse_regex !== undefined){
+        data = parse_regex.exec(data);
     }
-    console.debug("end api_prod_callback");
+    return data;
 }
 
 
 /** 
- * 爬取 pchome 商品 API 前 k 筆是有效的
- * 該 function 會呼叫 PROD_API_URL / PROD_API_URL_RELEASE
- * @params {int} count 有效商品筆數
+ * 商品資料爬蟲
+ * NOTE: 該 function 改動到 data_cache, 因為會直接新增該次爬回來的商品資料進去
+ * @param {object} data 該次 call API 拿回來的商品資料, 根據 pchome API 的 PROD_API_URL 來的
+ * @param {string} 該次爬取的主題, 指的是 url 所代表的商品類型(應該只傳 id 過來)
  */ 
-async function api_prod_count_callback(count){
-    console.debug("start api_prod_count_callback");
-    prod_count = count;
-    let epochs = Math.ceil(count / PROD_LIMIT)
-    for (let i = 0; i < epochs ; i = i + 1){
-        let prod_url = null;
-        prod_url = PROD_API_URL.replace(REPLACE_CATEGORY, category_id).replace(REPLACE_OFFSET, i * PROD_LIMIT);
-        let data = await _call_api(prod_url, PROD_REGEX);
-        api_prod_callback(JSON.parse(data));
+function add_prod_to_data_cache(data, category_id){
+    console.debug("start add_prod_to_data_cache");
+    for (let j = 0; j < data.length; j = j + 1){
+        if (!(category_id in data_cache)){
+            data_cache[category_id] = [];
+        }
+        data_cache[category_id].push(data[j]);
     }
-    console.debug("end api_prod_count_callback");
+    console.debug("end add_prod_to_data_cache");
 }
 
 
 /** 
  * 該程式爬取 pchome 商品 API 的主要呼叫 function
  * 該 function 會呼叫 PROD_COUNT_API_URL
- * @params {string} id 主題 id
+ * @param {string} id 主題 id
  */ 
 async function pchome_crawler(id){
     console.debug("start pchome_crawler");
-    category_id = id;
-    let prod_count_url = PROD_COUNT_API_URL.replace(REPLACE_CATEGORY, category_id);
-    let count = await _call_api(prod_count_url, PROD_COUNT_REGEX);
-    await api_prod_count_callback(parseInt(count));
+    console.debug(`id: ${id}`);
+    if (id in data_cache){
+        console.debug(`${id} data in cache`);
+    }else{
+        let prod_count_url = PROD_COUNT_API_URL.replace(REPLACE_CATEGORY, id);
+        let count = await _call_api(prod_count_url, PROD_COUNT_REGEX);
+        console.debug(`count: ${count}`);
+        
+        console.debug("start api_prod");
+        let epochs = Math.ceil(count / PROD_LIMIT)
+        for (let i = 0; i < epochs ; i = i + 1){
+            let prod_url = null;
+            prod_url = PROD_API_URL.replace(REPLACE_CATEGORY, id).replace(REPLACE_OFFSET, i * PROD_LIMIT);
+            let data = await _call_api(prod_url, PROD_REGEX);
+            add_prod_to_data_cache(JSON.parse(data), id);
+        }
+        console.debug("end api_prod");
+    }
+    console.debug("end pchome_crawler");
 }
 
 
 /**
  * API - Create
- * @params {string} id 商品 ID
- * @returns object API response
+ * @param {string} id 商品 ID
+ * @return {object} API response
  */
-async function _add(id){
-    if (all_intersection_id_array.indexOf(id) == -1){
-        all_intersection_id_array.push(id);
+async function _add(key, id){
+    if (!(key in all_intersection_id_array)){
+        all_intersection_id_array[key] = [];
+    }
+
+    if (all_intersection_id_array[key].indexOf(id) == -1){
+        all_intersection_id_array[key].push(id);
     }
     
-    console.log(`start crawl id: ${id}`);
     await pchome_crawler(id);
-    console.log(`end crawl id: ${id}`);
+
     return {success: true, action: "add"};
 }
 
 
 /**
  * API - Read
- * @returns object API response
+ * @return {object} API response
  */
-function _get(){
+function _get(key){
     let result = {};
-    result = intersection(result, crawler_data, all_intersection_id_array);
+    if (key in all_intersection_id_array){
+        result = intersection(result, data_cache, all_intersection_id_array[key]);
+    }
     return {result: result, success: true, action: "get"};
 }
 
 
 /**
  * API - Delete
- * @params {string} id 商品 ID
- * @returns object API response
+ * @param {string} id 商品 ID
+ * @return {object} API response
  */
-function _delete(id){
-    let index = all_intersection_id_array.indexOf(id);
-    all_intersection_id_array.splice(index, 1);
-    delete crawler_data[id];
+function _delete(key, id){
+    if (key in all_intersection_id_array){
+        let index = all_intersection_id_array[key].indexOf(id);
+        all_intersection_id_array[key].splice(index, 1);
+    }
     return {success: true, action: "delete"};
-}
-
-
-/**
- * check client url
- * @params {string} id 商品 ID
- * @returns object API response
- */
-function _check_same_url(new_url){
-    if (new_url === origin_url){
-        return true;
-    }
-    else{
-        origin_url = new_url;
-        return false;
-    }
-}
-
-
-/**
- * program variable reset
- */
-function _reset(){
-    crawler_data = {};
-    all_intersection_id_array = [];
-    all_union_id_array = [];
 }
 
 
@@ -275,33 +248,30 @@ chrome.runtime.onConnect.addListener(
         console.debug("Get connect");
         port.onMessage.addListener(
             async function(message){
-                // 先確認當前網址是否有改變
-                if (!_check_same_url(message.url)){
-                    // 若有改變(get false)則需要 reset
-                    console.debug("reset");
-                    _reset();
-                }
-
                 let result = {success: false, action: message.action};
                 try{
                     if (message.action === "add"){  // 增加 id
                         console.debug("add");
-                        result = await _add(message.id);
+                        result = await _add(message.url, message.id);
                     }
 
                     else if (message.action === "get"){  // 取得結果
                         console.debug("get");
-                        result = _get();
+                        result = _get(message.url);
                     }
 
                     else if (message.action === "delete"){  // 刪除 id
                         console.debug("delete");
-                        result = _delete(message.id);
+                        result = _delete(message.url, message.id);
                     }
 
                     else if (message.action === "get_intersection"){  // 取得 intersection array
                         console.debug("get_intersection");
-                        result = {result: all_intersection_id_array, success: true, action: "get_intersection"};
+                        if (message.url in all_intersection_id_array){
+                            result = {result: all_intersection_id_array[message.url], success: true, action: "get_intersection"};
+                        }else{
+                            result = {result: [], success: true, action: "get_intersection"};
+                        }
                     }
                 }
                 catch (e){
@@ -344,16 +314,17 @@ chrome.contextMenus.onClicked.addListener((info, tab)=>{
         return; 
     }
     console.log("產生報表");
+    let url = new URL(tab.url);
+    let path_name = new URL(url).pathname.split("/");
+    let url_id = path_name[path_name.length - 1];
+    url = new URL(url.pathname, url.origin).href;
+    console.log(`url: ${url}`);
+    console.log(`url_id: ${url_id}`);
 
-    let query = "";
-    for (let i = 0; i < all_intersection_id_array.length; i = i + 1){
-        let id = all_intersection_id_array[i];
-        if (query === ""){
-            query += id;
-        }
-        else{
-            query += `,${id}`;
-        }
+    let query = `${url_id}`;
+    for (let i = 0; i < all_intersection_id_array[url].length; i = i + 1){
+        let id = all_intersection_id_array[url][i];
+        query += `,${id}`;
     }
 
     chrome.tabs.create({
@@ -362,4 +333,4 @@ chrome.contextMenus.onClicked.addListener((info, tab)=>{
 });
 
 
-// TODO: Tabs close event 需要清除crawler_data
+// TODO: Tabs close event 需要清除data_cache
